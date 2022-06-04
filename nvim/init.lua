7b3d2d932e6aba39f82032984866f30450b92d9e
Cameron S. Williamson <therealkey@outlook.com>
2022-06-03 17:18:59 -0700

    Moved all bindings to their own file

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,7 @@
 vim.cmd("source ~/.config/nvim/utils.vim")
-local use = require('plugins')
 require('lsp')
+require('bindings')
+local use = require('plugins')
 --vim.cmd("colorscheme everblush")
 require('onedarkpro').load()
 vim.g.mapleader = ","
@@ -10,53 +11,4 @@ require('lualine').setup({options = { theme = 'everblush' }})
 require'nvim-tree'.setup {}
 require'hop'.setup()
 require('rust-tools').setup({})
-
---[
--- Mapping Functions
---]
-function map(mode, shortcut, command)
-    vim.api.nvim_set_keymap(mode, shortcut, command, { noremap = true, silent = true })
-end
-
--- Normal Mode mapping
-function nmap(shortcut, command, silent)
-    map('n', shortcut, command)
-end
-
--- Insert Mode mapping
-function imap(shortcut, command)
-    map('i', shortcut, command)
-end
-
--- Visaul Mode mapping
-function vmap(shortcut, command)
-    map('v', shortcut, command)
-end
-
---[
--- Bindings
---]
-
--- Telescope
-nmap("<leader>ff", "<cmd>Telescope find_files<cr>")
-nmap("<leader><tab>", "<cmd>Telescope find_files<cr>") 
-nmap("<leader>fg", "<cmd>Telescope live_grep<cr>")
-nmap("<leader>fb", "<cmd>Telescope buffers<cr>")
-nmap("<leader>fh", "<cmd>Telescope help_tags<cr>")
--- Reload init.lua / init.vim
-nmap("<leader>ll", "<cmd>source ~/.config/nvim/init.lua<cr>")
--- Nvim tree
-nmap("<C-n>", "<cmd>NvimTreeToggle<cr>")
-nmap("R", "<cmd>NvimTreeRefresh<cr>")
--- Hop
-nmap("<leader>w", "<cmd>HopWord<cr>")
-nmap("<leader>j", "<cmd>HopLine<cr>")
--- Misc
-nmap("<leader>t", "<C-w>s <C-w>j <cmd>term<cr><C-w>20-")
-nmap("<leader>u", "20<C-w>-")
-imap("jk", "<esc>")
-nmap("<C-h>", "<C-w>h")
-nmap("<C-j>", "<C-w>j")
-nmap("<C-k>", "<C-w>k")
-nmap("<C-l>", "<C-w>l")
 
---
+++ nvim/lua/bindings.lua
@@ -0,0 +1,34 @@
+function map(mode, shortcut, command)
+    vim.api.nvim_set_keymap(mode, shortcut, command, { noremap = true, silent = true })
+end
+function nmap(shortcut, command, silent)
+    map('n', shortcut, command)
+end
+function imap(shortcut, command)
+    map('i', shortcut, command)
+end
+function vmap(shortcut, command)
+    map('v', shortcut, command)
+end
+-- Telescope
+nmap("<leader>ff", "<cmd>Telescope find_files<cr>")
+nmap("<leader><tab>", "<cmd>Telescope find_files<cr>") 
+nmap("<leader>fg", "<cmd>Telescope live_grep<cr>")
+nmap("<leader>fb", "<cmd>Telescope buffers<cr>")
+nmap("<leader>fh", "<cmd>Telescope help_tags<cr>")
+-- Reload init.lua / init.vim
+nmap("<leader>ll", "<cmd>source ~/.config/nvim/init.lua<cr>")
+-- Nvim tree
+nmap("<C-n>", "<cmd>NvimTreeToggle<cr>")
+nmap("R", "<cmd>NvimTreeRefresh<cr>")
+-- Hop
+nmap("<leader>w", "<cmd>HopWord<cr>")
+nmap("<leader>j", "<cmd>HopLine<cr>")
+-- Misc
+nmap("<leader>t", "<C-w>s <C-w>j <cmd>term<cr><C-w>20-")
+nmap("<leader>u", "20<C-w>-")
+imap("jk", "<esc>")
+nmap("<C-h>", "<C-w>h")
+nmap("<C-j>", "<C-w>j")
+nmap("<C-k>", "<C-w>k")
+nmap("<C-l>", "<C-w>l")
