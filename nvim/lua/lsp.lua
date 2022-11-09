a5136d4732dced589ab5d0193f13465769da6cbe
Cameron S. Williamson <therealkey@outlook.com>
2022-11-09 11:26:48 -0800

    fixed 'update_capabilities is depricated' error on launch

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -13,7 +13,7 @@ lsp_installer.setup{}
 -- Configure Snippets
 --]
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 local lspconfig = require('lspconfig')
 
