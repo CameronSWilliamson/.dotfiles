7ddb08d01cf59978d92e8938dae4418fb7452e02
Cameron S. Williamson <therealkey@outlook.com>
2023-03-19 02:15:27 -0700

    allowing for 'other' as an option to cheetsheet something new

--- scripts/cht.sh
+++ scripts/cht.sh
@@ -5,12 +5,21 @@
 languages=`echo "golang lua cpp c typescript nodejs rust python" | tr ' ' '\n'`
 core_utils=`echo "xargs find mv sed awk grep tmux" | tr ' ' '\n'`
 
-selected=`printf "$languages\n$core_utils" | fzf`
+selected=`printf "other\n$languages\n$core_utils" | fzf`
+if [ "$selected" == "other" ]; then
+    read -p "subject: " selected
+fi
 read -p "query: " query
 
-if printf $languages | grep -qs $selected
-then
+echo $selected
+
+if printf $languages | grep -qs $selected; then
     tmux neww bash -c "curl cht.sh/$selected/`echo $query | tr ' ' '+'` & while [ : ]; do sleep 1; done"
 else
-    curl cht.sh/$selected~$query & while [ : ]; do sleep 1; done
+
+    if [ -z "$query" ]; then
+        curl cht.sh/$selected & while [ : ]; do sleep 1; done
+    else
+        curl cht.sh/$selected~$query & while [ : ]; do sleep 1; done
+    fi
 fi
