5fed1b2c320e6673615ce2395dc198409f60aa57
Cameron S. Williamson <therealkey@outlook.com>
2023-04-06 16:12:00 -0700

    Added highlight on save and removed netrw banner

--- nvim/lua/key/init.lua
+++ nvim/lua/key/init.lua
@@ -1,3 +1,20 @@
 require("key.packer")
 require("key.remap")
 require("key.set")
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+local yank = augroup("HighlightYank", {})
+
+autocmd('TextYankPost', {
+    group = yank,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 100,
+        })
+    end,
+})
+
+vim.g.netrw_banner = 0
