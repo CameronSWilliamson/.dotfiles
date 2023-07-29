ebe8262efe3a00e04011546022a1716aecfc0a17
Cameron S. Williamson <therealkey@outlook.com>
2023-07-29 12:40:49 -0700

    Added bigrams

--- config/nvim/after/plugin/hop.lua
+++ config/nvim/after/plugin/hop.lua
@@ -4,4 +4,9 @@ vim.keymap.set('n', '<leader>s', function()
         hop.hint_char1()
     end
 )
+
+vim.keymap.set('n', '<leader><leader>s', function ()
+        hop.hint_char2()
+    end
+)
 hop.setup()
